72da507c9e12f6d9ea3f569dfe52fbee61f7f4cb
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-11 12:00:00 +0530

    minor: expand date format in status bar

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -999,10 +999,10 @@ public class AttendanceCalculator extends JFrame {
         add(statusBar, BorderLayout.SOUTH);
 
         javax.swing.Timer clockTimer = new javax.swing.Timer(1000, e -> {
-            statusTimeLabel.setText(java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("EEE, MMM d, yyyy")) + " | " + java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
+            statusTimeLabel.setText(java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy")) + " | " + java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
         });
         clockTimer.start();
-        statusTimeLabel.setText(java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("EEE, MMM d, yyyy")) + " | " + java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
+        statusTimeLabel.setText(java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy")) + " | " + java.time.LocalTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss")));
 
         // Action Listeners
         calculateButton.addActionListener(e -> calculateAndAdd());
