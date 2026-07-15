ae87e711d800b834c06607e4bca9bd78477911e2
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-15 23:49:31 +0530

    Update 50: Add MSG_CLEAR_CONFIRM constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -70,6 +70,7 @@ public class AttendanceCalculator extends JFrame {
     private static final String MSG_UNSAVED_DATA = "You have unsaved data. Save before loading?";
     private static final String MSG_LOAD_CONFIRM = "Loading will replace current data. Continue?";
     private static final String MSG_DELETE_CONFIRM = "Are you sure you want to delete %d row(s)?";
+    private static final String MSG_CLEAR_CONFIRM = "Are you sure you want to clear all data?";
     private static final Color COLOR_HEADER = new Color(41, 128, 185);
     private static final Color COLOR_SAFE = new Color(39, 174, 96);
     private static final Color COLOR_WARNING = new Color(241, 196, 15);
