790fe692855af2ba60a2e95acbab64ae005f52a2
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-16 23:35:33 +0530

    Update 81: Add AUTO_SAVE_INTERVAL constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -113,6 +113,7 @@ public class AttendanceCalculator extends JFrame {
     private static final String PERCENTAGE_DISPLAY_FORMAT = "%.2f%%";
     private static final int BUFFER_SIZE = 1024;
     private static final int MAX_BACKUP_COUNT = 5;
+    private static final int AUTO_SAVE_INTERVAL = 300000;
     private JTextField subjectField;
     private JTextField totalClassesField;
     private JTextField attendedClassesField;
