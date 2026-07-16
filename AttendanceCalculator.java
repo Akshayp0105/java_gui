8c3cc85391058e6941bda6bb71f53464105bfe92
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-16 23:33:39 +0530

    Update 72: Add APP_LICENSE constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -104,6 +104,7 @@ public class AttendanceCalculator extends JFrame {
     private static final int MIN_WINDOW_WIDTH = 720;
     private static final int MIN_WINDOW_HEIGHT = 460;
     private static final String APP_WEBSITE = "https://github.com/LOQ/attendance-calculator";
+    private static final String APP_LICENSE = "MIT";
     private JTextField subjectField;
     private JTextField totalClassesField;
     private JTextField attendedClassesField;
