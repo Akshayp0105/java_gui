ee5f9aeed05574c5e263167b173c9157cd245b6d
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-16 23:37:01 +0530

    Update 89: Add TOOLTIP_DISMISS_DELAY constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -121,6 +121,7 @@ public class AttendanceCalculator extends JFrame {
     private static final int TABLE_ROW_HEIGHT = 32;
     private static final int SCROLL_UNIT_INC = 16;
     private static final int TOOLTIP_INIT_DELAY = 500;
+    private static final int TOOLTIP_DISMISS_DELAY = 10000;
     private JTextField subjectField;
     private JTextField totalClassesField;
     private JTextField attendedClassesField;
