95c0989f83543df3b9d53ab1fdf19776ae0fc289
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-16 23:35:55 +0530

    Update 83: Add LOG_FILE_NAME constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -115,6 +115,7 @@ public class AttendanceCalculator extends JFrame {
     private static final int MAX_BACKUP_COUNT = 5;
     private static final int AUTO_SAVE_INTERVAL = 300000;
     private static final String CONFIG_FILE_NAME = "config.properties";
+    private static final String LOG_FILE_NAME = "app.log";
     private JTextField subjectField;
     private JTextField totalClassesField;
     private JTextField attendedClassesField;
