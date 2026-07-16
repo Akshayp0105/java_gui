a9964921014fddb2a872a6349f3d902548fc5069
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-16 23:35:21 +0530

    Update 80: Add MAX_BACKUP_COUNT constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -112,6 +112,7 @@ public class AttendanceCalculator extends JFrame {
     private static final String TIME_FORMAT = "HH:mm:ss";
     private static final String PERCENTAGE_DISPLAY_FORMAT = "%.2f%%";
     private static final int BUFFER_SIZE = 1024;
+    private static final int MAX_BACKUP_COUNT = 5;
     private JTextField subjectField;
     private JTextField totalClassesField;
     private JTextField attendedClassesField;
