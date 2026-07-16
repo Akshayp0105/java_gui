2ba0132a5ded733f053310a75880545400bbf9c1
Akshayp0105 <lifehacker12468@gmail.com>
2026-07-16 23:37:34 +0530

    Update 92: Add GREETING_EVENING constant

--- AttendanceCalculator.java
+++ AttendanceCalculator.java
@@ -124,6 +124,7 @@ public class AttendanceCalculator extends JFrame {
     private static final int TOOLTIP_DISMISS_DELAY = 10000;
     private static final String GREETING_MORNING = "Good Morning";
     private static final String GREETING_AFTERNOON = "Good Afternoon";
+    private static final String GREETING_EVENING = "Good Evening";
     private JTextField subjectField;
     private JTextField totalClassesField;
     private JTextField attendedClassesField;
